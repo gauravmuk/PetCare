4a8ef5dc1dec5a94d7ce25c8535989199df4d60c
tzuchiangweng <lisa.weng@mail.utoronto.ca>
2016-03-19 23:56:50 -0400

    add get user by id

--- PetCare/public/javascripts/app.js
+++ PetCare/public/javascripts/app.js
@@ -2,4 +2,13 @@ var app = angular.module('petCare', []);
 
 app.controller('userController', function() {
 
+});app.controller('mainController', function() {
 });
+
+app.controller('userController', ['$http', '$scope', function($http, $scope) {
+	$scope.user = []
+	$http.get('/users/1').success(function(data){
+		console.log(data);
+		$scope.user = data;
+	});
+}]);
