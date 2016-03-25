11ead25e57173599d37f04bdd777754d8c61c873
tzuchiangweng <lisa.weng@mail.utoronto.ca>
2016-03-24 21:38:55 -0400

    integrate basic routing

--- PetCare/public/javascripts/app.js
+++ PetCare/public/javascripts/app.js
@@ -1,14 +1,210 @@
-var app = angular.module('petCare', []);
+var app = angular.module('petCare', ['ngRoute']);
 
-app.controller('userController', function() {
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+	$locationProvider.html5Mode(true);
 
-});app.controller('mainController', function() {
+	$routeProvider
+		.when('/', {
+			templateUrl: 	'/layouts/home.html',
+			controller: 	'mainController',
+			controllerAs: 	'mainCtrl'
+		})
+		.when('/users/:id', {
+			templateUrl: 	'/users/show.html',
+			controller: '	userController',
+			controllerAs: 	'userCtrl'
+		})
+		.when('/users/:id/applications', {
+			templateUrl: 	'/users/applications.html',
+			controller: 	'userController',
+			controllerAs: 	'userCtrl'
+		})
+		.when('/users/:id/messages', {
+			templateUrl: 	'/users/messages.html',
+			controller: 	'userController',
+			controllerAs: 	'userCtrl'
+		})
+		.when('/signin', {
+			templateUrl: 	'/signin.html',
+			controller: 	'userController',
+			controllerAs: 	'userCtrl'
+		})
+		.when('/signup', {
+			templateUrl: 	'/signup.html',
+			controller: 	'userController',
+			controllerAs: 	'userCtrl'
+		})
+		.when('/pet_posts', {
+			templateUrl: 	'/pet_posts/index.html',
+			controller: 	'petPostingController',
+			controllerAs: 	'petPostingCtrl'
+		})
+		.when('/new_pet_posts', {
+			templateUrl: 	'/pet_posts/new.html',
+			controller: 	'petPostingFormController',
+			controllerAs: 	'petPostingFormCtrl'
+		})
+		.when('/pet_posts/:id', {
+			templateUrl: 	'/pet_posts/show.html',
+			controller: 	'petPostingController',
+			controllerAs: 	'petPostingCtrl'
+		})
+		.when('/petsitter_posts', {
+			templateUrl: 	'/petsitter_posts/index.html',
+			controller: 	'sitterPostingController',
+			controllerAs: 	'sitterPostingCtrl'
+		})
+		.when('/petsitter_posts/:id', {
+			templateUrl: 	'/petsitter_posts/show.html',
+			controller: 	'sitterPostingController',
+			controllerAs: 	'sitterPostingCtrl'
+		})
+		.when('/new_petsitter_posts', {
+			templateUrl: 	'/petsitter_posts/new.html',
+			controller: 	'petPostingFormController',
+			controllerAs: 	'petPostingFormCtrl'
+		});
+}]);
+
+app.controller('mainController', function() {
+});
+
+app.controller('adminController', function() {
 });
 
 app.controller('userController', ['$http', '$scope', function($http, $scope) {
 	$scope.user = []
-	$http.get('/users/1').success(function(data){
+	$http.get('/api/users/1').success(function(data){
 		console.log(data);
 		$scope.user = data;
 	});
+}]);
+
+app.controller('reviewController', ['$http', '$scope', function($http, $scope) {
+	
+	var rating;
+
+	$scope.submitStar = function(rating){
+		// Set rating to the rating level user selected
+		this.rating = rating;
+	}
+
+	$scope.submitReview = function (){
+		// Another way to check for undefined/null/NaN
+		if(!Number.isInteger(this.rating))
+			this.rating = 0;
+
+		var reviewComment = $scope.reviewComment;
+		var reviewRating = this.rating;
+
+		// Creare object to be sent through the post request
+		var dataObj = {
+    		to: 1,
+			from: 2,
+			rating: reviewRating,
+			comment: reviewComment
+		}
+
+		// Make a http post request to the server
+		console.log("Make a post request to /reviews");
+		$http.post('/api/reviews', {data:dataObj})
+			.success(function(data, status, headers, config) {
+    			// TO-DO: Get the average rating
+			}).error(function(data, status, headers, config) {
+    			
+			});
+	};
+}]);
+
+app.controller('petPostingController', ['$http', '$scope', function($http, $scope) {
+	
+	$scope.petPosting = []
+	$scope.pet = []
+
+	$http.get('/api/petpostings/1').success(function(data) {
+		$scope.petPosting = data;
+
+		// get the posting's pet number
+		var petID = $scope.petPosting.pet;
+
+		// make AJAX call to get pet info
+		$http.get('/api/pets/' + petID).success(function(data) {
+			$scope.pet = data;
+		});
+
+	});
+
+}]);
+
+
+app.controller('sitterPostingController', ['$http', '$scope', function($http, $scope) {
+	
+	$scope.sitterPosting = []
+
+	$http.get('/api/sitterpostings/1').success(function(data) {
+		$scope.sitterPosting = data;
+	});
+
+}]);
+
+app.controller('sitterPostingFormController', ['$http', '$scope', function($http, $scope) {
+
+	$scope.createPosting = function (){
+
+		// Create object to be sent through the POST request
+		var dataObj = {
+		    user: 1,	// TODO: Use user id from session
+			title: $scope.title,
+			types: $scope.types,
+			duration: $scope.duration,
+			location: $scope.location,
+			price: $scope.price,
+			experience: $scope.experience,
+			supplies: $scope.supplies,
+			number_of_pets: $scope.number_of_pets,
+			description: $scope.description,
+			thumbnail: '/images/default-profile-pic.png',	// TODO: Get user image
+			status: 'open',
+		};
+
+		// Make POST request to the /sitterpostings
+		$http.post('/api/sitterpostings', {data: dataObj})
+			.success(function(data, status, headers, config) {
+
+			}).error(function(data, status, headers, config) {
+    			
+			});
+	};
+
+}]);
+
+app.controller('petPostingFormController', ['$http', '$scope', function($http, $scope) {
+
+	$scope.createPosting = function (){
+
+		console.log("creating pet posting");
+
+		// Create object to be sent through the POST request
+		var dataObj = {
+		    user: 1,	// TODO: Use user id from session
+    		pet: 1,		// TODO: Use actual pet id
+			title: $scope.title,
+			duration: $scope.duration,
+			location: $scope.location,
+			price: $scope.price,
+			supplies: $scope.supplies,
+			additional_info: $scope.additional_info,
+			description: $scope.description,
+			thumbnail: '/images/cat1.png',	// TODO: Get user image
+			status: 'open',
+		};
+
+		// Make POST request to the /petpostings
+		$http.post('/api/petpostings', {data: dataObj})
+			.success(function(data, status, headers, config) {
+
+			}).error(function(data, status, headers, config) {
+    			
+			});
+	};
 }]);
--- PetCare/server.js
+++ PetCare/server.js
@@ -56,48 +56,57 @@ var Dog = mongoose.model("Dog", dogSchema);
 
 
 /* Application Routes */
-
-app.get("/", function(req, res){
-	res.render("index.html");
+app.get("/layouts/home.html",function(req,res){
+    res.render("layouts/home.html");
 });
 
-app.get("/signup", function(req, res){
-	res.render("signup.html");
+app.get("/users/show.html", function(req, res) {
+	res.render("users/show.html")
 });
 
-app.get("/signin", function(req, res){
-	res.render("signin.html");
+app.get("/users/applications.html", function(req, res) {
+	res.render("users/applications.html")
 });
 
-app.get("/admin", function(req, res){
-	res.render("admin.html");
+app.get("/users/messages.html", function(req, res) {
+	res.render("users/messages.html")
 });
 
-app.get("/new_post_pet", function(req, res){
-	res.render("new_post_pet.html");
+app.get("/signin.html", function(req, res) {
+	res.render("signin.html")
 });
 
-app.get("/new_post_petsitter", function(req, res){
-	res.render("new_post_petsitter.html");
+app.get("/signup.html", function(req, res) {
+	res.render("signup.html")
 });
 
-app.get("/post_pet", function(req, res){
-	res.render("post_pet.html");
+app.get("/pet_posts/index.html", function(req, res){
+	res.render("pet_posts/index.html");
 });
 
-app.get("/post_petsitter", function(req, res){
-	res.render("post_petsitter.html");
+app.get("/pet_posts/new.html", function(req, res){
+	res.render("pet_posts/new.html");
 });
 
-app.get("/search_pet", function(req, res){
-	res.render("search_pet.html");
+app.get("/pet_posts/show.html", function(req, res){
+	res.render("pet_posts/show.html");
 });
 
-app.get("/search_petsitter", function(req, res){
-	res.render("search_petsitter.html");
+app.get("/petsitter_posts/index.html", function(req, res){
+	res.render("petsitter_posts/index.html");
 });
 
-app.get("/users/:id", function(req, res){
+app.get("/petsitter_posts/show.html", function(req, res){
+	res.render("petsitter_posts/show.html");
+});
+
+app.get("/petsitter_posts/new.html", function(req, res){
+	res.render("petsitter_posts/new.html");
+});
+
+
+/* REST API routes */
+app.get("/api/users/:id", function(req, res){
 	var user = [];
 	User.findById(req.params.id, function(err, user) {
 		if (err) {
@@ -107,19 +116,95 @@ app.get("/users/:id", function(req, res){
 	});
 });
 
-app.get("/user_profile", function(req, res){
-	res.render("user_profile.html");
+app.post("/api/reviews", function(req, res){
+
+	var toUser 			= req.body.data.to;
+	var fromUser 		= req.body.data.from;
+	var reviewRating 	= req.body.data.rating;
+	var reviewComment 	= req.body.data.comment;
+
+	console.log("Review Rating : "+ reviewRating);
+
+	// Save to the user reviwe information to the database
+	// TO-DO: Also calclute the average rating and save under user.rating
+	// 		  Send avg number back to front-end as res.json()
+	// Review.create({
+	// 	to: user1._id,
+	// 	from: user2._id,
+	// 	rating: 3,
+	// 	comment: "reviewComment"
+
+	// }, function(err, review){
+	// 	if(err){
+	// 		console.log("Review.create(): error = "+ err);
+	// 	}
+	// 	else{
+	// 		console.log("Review.create(): successful");
+	// 		console.log(review);
+	// 	}
+	// });
+
+	// Send back a response or end response
+	res.json({resData: "data"});
+	// res.end();
+});
+
+app.get("/api/pets/:id", function(req, res){
+	var pet = [];
+	Pet.findById(req.params.id, function(err, pet) {
+		if (err) {
+			throw err;
+		}
+		res.json(pet)
+	});
+});
+
+app.get("/api/petpostings/:id", function(req, res){
+	var petposting = [];
+	Pet_Posting.findById(req.params.id, function(err, petposting) {
+		if (err) {
+			throw err;
+		}
+		res.json(petposting)
+	});
+});
+
+app.post("/api/petpostings", function(req, res){
+
+	console.log("New Pet Post");
+	console.log(req.body);
+	res.json({resData: "data"});
+
+});
+
+app.get("/api/sitterpostings/:id", function(req, res){
+	var sitterposting = [];
+	Sitter_Posting.findById(req.params.id, function(err, sitterposting) {
+		if (err) {
+			throw err;
+		}
+		res.json(sitterposting)
+	});
+});
+
+app.post("/api/sitterpostings", function(req, res){
+
+	console.log("New Sitter Post");
+	console.log(req.body);
+	res.json({resData: "data"});
+
+>>>>>>> Stashed changes
 });
 
 // Tesing 
-app.get("/dogs", function(req, res){
+app.get("/api/dogs", function(req, res){
 	res.send("Who let the dogs out?");
 });
 
 // Testing Route Parameters
 // Ex: 	/http://localhost:3000/dogs/1
 // 		/http://localhost:3000/dogs/33
-app.get("/dogs/:id", function(req, res){
+app.get("/api/dogs/:id", function(req, res){
 	console.log(req.params);
 	res.send("Dog id = "+ req.params.id);
 });
@@ -127,7 +212,7 @@ app.get("/dogs/:id", function(req, res){
 // Testing Route Parameters + Database
 // Ex: 	/http://localhost:3000/dogs/new/Timmy
 // 		/http://localhost:3000/dogs/new/Max
-app.get("/dogs/new/:name", function(req, res){
+app.get("/api/dogs/new/:name", function(req, res){
 	var dogName = req.params.name;
 
 	// Adding a new dog to the database testDB
@@ -151,7 +236,7 @@ app.get("/dogs/new/:name", function(req, res){
 // Testing Route Parameters + Database
 // Ex: 	/http://localhost:3000/dogs/find/Timmy
 // 		/http://localhost:3000/dogs/find/Buster
-app.get("/dogs/find/:name", function(req, res){
+app.get("/api/dogs/find/:name", function(req, res){
 	var dogName = req.params.name;
 
 	var result = [];
@@ -172,6 +257,9 @@ app.get("/dogs/find/:name", function(req, res){
 			}
 	});
 
+});
+app.use("*",function(req,res){
+    res.sendFile(path.join(__dirname,"views/index.html"));
 });
 
 // If none of the above routes matches, display an error
