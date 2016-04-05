20a899f9bffe2473cd0c4105e7c24e33b986612c
tzuchiangweng <lisa.weng@mail.utoronto.ca>
2016-04-05 03:21:44 -0400

    change callback urls to optional string

--- public/javascripts/fb_authenticate.js
+++ public/javascripts/fb_authenticate.js
@@ -1,5 +1,5 @@
 module.exports = {
 	'appID'			: '605409222968354',
 	'appSecret' 	: '0b67d68f6edced848b030bd8e1ccf5a3',
-	'callbackUrl' 	: 'http://localhost:3000/auth/facebook/callback'
+	'callbackUrl' 	: process.env.FB_CALLBACK_URL || 'http://localhost:3000/auth/facebook/callback'
 }
