0a8d4900613d0c8169f483c11380a41c5fa55bb8
davidla25 <davidla25@gmail.com>
2016-04-05 17:46:46 -0400

    update default-data.js

--- default-data.js
+++ default-data.js
@@ -30,17 +30,17 @@ var connection = mongoose.connect(uristring);
 autoIncrement.initialize(connection);
 
 // Import Database schema
-var Application 	= require(__dirname + '/public/models/Application');
-var Message 		= require(__dirname + '/public/models/Message');
-var Pet 			= require(__dirname + '/public/models/Pet');
-var Pet_Posting 	= require(__dirname + '/public/models/Pet_Posting');
-var Sitter_Posting	= require(__dirname + '/public/models/Sitter_Posting');
-var Report			= require(__dirname + '/public/models/Report');
-var Review			= require(__dirname + '/public/models/Review');
-var Pet_Review		= require(__dirname + '/public/models/Pet_Review');
-var User			= require(__dirname + '/public/models/User');
-var ForumPost		= require(__dirname + '/public/models/Forum_Post');
-var Authentication  = require(__dirname + '/public/models/Authentication');
+var Application 	= require(__dirname + '/server/models/Application');
+var Message 		= require(__dirname + '/server/models/Message');
+var Pet 			= require(__dirname + '/server/models/Pet');
+var Pet_Posting 	= require(__dirname + '/server/models/Pet_Posting');
+var Sitter_Posting	= require(__dirname + '/server/models/Sitter_Posting');
+var Report			= require(__dirname + '/server/models/Report');
+var Review			= require(__dirname + '/server/models/Review');
+var Pet_Review		= require(__dirname + '/server/models/Pet_Review');
+var User			= require(__dirname + '/server/models/User');
+var ForumPost		= require(__dirname + '/server/models/Forum_Post');
+var Authentication  = require(__dirname + '/server/models/Authentication');
 
 // Drop all collections in the db
 User.collection.drop();
