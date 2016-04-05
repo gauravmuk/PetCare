6fdf013211e76d2a3d994b26d2c66d5f8b9a4641
tzuchiangweng <lisa.weng@mail.utoronto.ca>
2016-04-05 04:27:48 -0400

    add test twitter key

--- public/javascripts/fb_authenticate.js
+++ public/javascripts/fb_authenticate.js
@@ -1,5 +1,5 @@
 module.exports = {
 	'appID'			: process.env.FB_APP_ID || '509195005931801',
-	'appSecret' 	: process.env.FB_SECRET || 'e8a939fe8230ca0729a8279473ff0825',
+	'appSecret' 	: process.env.FB_APP_SECRET || 'e8a939fe8230ca0729a8279473ff0825',
 	'callbackUrl' 	: process.env.FB_CALLBACK_URL || 'http://localhost:3000/auth/facebook/callback'
 }
--- public/javascripts/twitter_authenticate.js
+++ public/javascripts/twitter_authenticate.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'consumerKey': 'FQ3KrnNIrBkMS2ypnXaFvzGY4',
-	'consumerSecret': 'mkfJJ2LCRoZ7hiRELAcaGLRQkAWjB3lczq4pffB9HQDrTnpDyw',
+	'consumerKey': process.env.TWITTER_CONSUMER_KEY || 'hiftMt3bBZ4deOswFQJXKsZpn',
+	'consumerSecret': process.env.TWITTER_CONSUMER_SECRET || '	kGBwlQdMT8geqFXqtM6wscdumxbVsr6xYfywnYbmCZBbPWg0ol',
 	'callbackUrl': process.env.TWITTER_CALLBACK_URL || 'http://localhost:3000/auth/twitter/callback'
 }
